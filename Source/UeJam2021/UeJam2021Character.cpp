f1d0ff0a0de71d0492783af3f12cafc27d2a2a8b
palicron <elgranpalicron@gmail.com>
2021-08-26 19:59:15 -0500

    Ground check y mejoras dahs

--- Source/UeJam2021/UeJam2021Character.cpp
+++ Source/UeJam2021/UeJam2021Character.cpp
@@ -1,14 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UeJam2021Character.h"
-#include "HeadMountedDisplayFunctionLibrary.h"
-#include "Camera/CameraComponent.h"
+
+#include "DrawDebugHelpers.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
-#include "MyBlueprintFunctionLibrary.h"
-#include "GameFramework/SpringArmComponent.h"
+#include "TimerManager.h"
+
 
 //////////////////////////////////////////////////////////////////////////
 // AUeJam2021Character
@@ -17,10 +17,6 @@ AUeJam2021Character::AUeJam2021Character()
 {
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
-
-	// set our turn rates for input
-	BaseTurnRate = 45.f;
-	BaseLookUpRate = 45.f;
 
 	// Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
@@ -33,16 +29,6 @@ AUeJam2021Character::AUeJam2021Character()
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
-	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
-	// Create a follow camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
@@ -64,46 +50,71 @@ void AUeJam2021Character::SetupPlayerInputComponent(class UInputComponent* Playe
 	PlayerInputComponent->BindAxis("MoveRight", this, &AUeJam2021Character::MoveRight);
 
 
-	
-	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
-	// "turn" handles devices that provide an absolute delta, such as a mouse.
-	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AUeJam2021Character::TurnAtRate);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis("LookUpRate", this, &AUeJam2021Character::LookUpAtRate);
-
-
 }
 
 void AUeJam2021Character::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-
-	//UE_LOG(LogTemp, Warning, TEXT("Current Velocity %s"),*GetVelocity().ToString());
+	CheckGround();
+	
 	
 }
 
 void AUeJam2021Character::MakeDash()
 {
+	//TODO Force depending of velocity
+	if(canDash)
+	{
+		FVector direction = GetActorForwardVector();
+		direction.Z = 0;
+		
+		if (isGrounded)
+		{
+			LaunchCharacter(direction.GetSafeNormal() * 1200.f, false, false);
+		}
+		else
+		{
+			LaunchCharacter(direction.GetSafeNormal() * 500.f, false, true);
+		}
+		canDash = false;
+		GetWorldTimerManager().SetTimer(DashTime, this, &AUeJam2021Character::ResetDash, DashCd);
+	}
+
 	
-
-	LaunchCharacter(GetActorForwardVector().GetSafeNormal()*1000.f, false, false);
-
 }
 
-
-void AUeJam2021Character::TurnAtRate(float Rate)
+void AUeJam2021Character::ResetDash()
 {
-	// calculate delta for this frame from the rate information
-	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	canDash = true;
+	
+	
 }
 
-void AUeJam2021Character::LookUpAtRate(float Rate)
+void AUeJam2021Character::CheckGround()
 {
-	// calculate delta for this frame from the rate information
-	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+	FHitResult hit;
+	FVector Star = GetActorLocation();
+	FVector End = Star + (-GetActorUpVector() * 100.f);
+
+	FCollisionQueryParams CollParams;
+
+	bool ground = GetWorld()->LineTraceSingleByChannel(hit, Star, End, ECC_Visibility, CollParams);
+
+	if(ground)
+	{
+		isGrounded = true;
+	}
+	else
+	{
+		isGrounded = false;
+	}
+
+	
+	//DrawDebugLine(GetWorld(),Star,End,FColor::Red,false,1,0,1);
+	
+	
 }
+
 
 void AUeJam2021Character::MoveForward(float Value)
 {
--- Source/UeJam2021/UeJam2021Character.h
+++ Source/UeJam2021/UeJam2021Character.h
@@ -11,23 +11,11 @@ class AUeJam2021Character : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+
+
 public:
 	AUeJam2021Character();
-
-	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseTurnRate;
-
-	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
-	float BaseLookUpRate;
 
 protected:
 
@@ -39,17 +27,6 @@ protected:
 	/** Called for side to side input */
 	void MoveRight(float Value);
 
-	/** 
-	 * Called via input to turn at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void TurnAtRate(float Rate);
-
-	/**
-	 * Called via input to turn look up/down at a given rate. 
-	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
-	 */
-	void LookUpAtRate(float Rate);
 
 
 protected:
@@ -60,13 +37,23 @@ protected:
 
 	void MakeDash();
 
+	FTimerHandle DashTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movment Stats")
+		float DashCd = 1.0f;
+
+	void ResetDash();
+
+	void CheckGround();
+	
+
 public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movment Stats")
 		float DashMulty = 2.0f;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movment Stats")
+	bool isGrounded = false;
+
+	bool canDash = true;
 };
 
