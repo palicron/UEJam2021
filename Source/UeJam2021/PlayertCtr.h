22134c690cb5bc41655ddc2a2491247ce0e62a91
palicron <elgranpalicron@gmail.com>
2021-08-29 00:14:23 -0500

    Add pause menu

---
+++ Source/UeJam2021/PlayertCtr.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayertCtr.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEJAM2021_API APlayertCtr : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
